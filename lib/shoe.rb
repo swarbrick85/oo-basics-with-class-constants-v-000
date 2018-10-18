89e2dc89225a38aaf20211393a166424a05dc5a1
David F Swarbrick Jr <david.swarbrick.jr@gmail.com>
2018-10-18 19:53:16 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,8 +2,26 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
+  BRANDS = []
+  
+
+  
   def initialize(brand)
     @brand = brand
+    x = 0
+    y = 0
+    while y < BRANDS.size 
+    if BRANDS[y] != brand 
+      x += 1
+      y += 1 
+    else 
+      y += 1 
+    end 
+    end 
+    if BRANDS.size == x
+      BRANDS <<  brand
+    end 
+    
   end
 
   def cobble
